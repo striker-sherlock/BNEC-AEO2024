ec5e558252337a4116110d7f6d6b439bd53688a6
geren <gerenwidiarta477@gmail.com>
2023-10-09 17:22:44 +0700

    feat:added saperated admin and PIC

--- database/migrations/2023_10_09_095308_create_admins_table.php
+++ database/migrations/2023_10_09_095308_create_admins_table.php
@@ -15,7 +15,20 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('created_by');
+            $table->timestamp('created_at')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->string('name');
+            $table->string('nim');
+            $table->string('email');
+            $table->integer('position_id');
+            $table->string('position');
+            $table->char('department_id', 5);
+            $table->string('department');
+            $table->char('division_id', 5);
+            $table->string('division');
+            $table->string('password');
         });
     }
 
